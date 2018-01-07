60270ba089832512f1b7af4224febc7150c29ffe
Robert <robert.pastor0691@orange.fr>
2018-01-07 20:02:55 +0100

    initial ace integration

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+import hello.views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', hello.views.index, name='hello-index'),
 ]
+
