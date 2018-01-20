8d7c4cd07d8089ef1ca946bf6b3189c238e45e43
Robert <robert.pastor0691@orange.fr>
2018-01-20 15:05:44 +0100

    settings

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -78,14 +78,6 @@ WSGI_APPLICATION = 'gettingstarted.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'calculator',
-        'USER': 'postgres',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    },
-    'sqlite': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
